ae2291812185011a232aaf77f887f895fedb06cf
ashe848 <ashe848@aucklanduni.ac.nz>
2016-10-07 21:42:39 +1300

    In the end made VoxButton and VoxMouseAdapter classes for text button defaults and button mouse listener

---
+++ VOXSPELLwithTheoAndAbby/src/windowbuilder/VoxButton.java
@@ -0,0 +1,31 @@
+package windowbuilder;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class VoxButton extends JButton {
+	private VoxMouseAdapter adapter;
+	
+	public VoxButton(String s){
+		super(s);
+		setForeground(Color.WHITE);
+		setBackground(new Color(254, 157, 79));
+		setContentAreaFilled(false);
+		setOpaque(true);
+		setFocusPainted(false);
+		setBorderPainted(false);
+		setupMouseListener();
+	}
+
+	private void setupMouseListener() {
+		adapter=new VoxMouseAdapter(this,Color.LIGHT_GRAY);
+		addMouseListener(adapter);
+	}
+	
+	public void changeMouseEventColor(Color c){
+		removeMouseListener(adapter);
+		addMouseListener(new VoxMouseAdapter(this,c));
+	}
+}
---
+++ VOXSPELLwithTheoAndAbby/src/windowbuilder/VoxMouseAdapter.java
@@ -0,0 +1,50 @@
+package windowbuilder;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+public class VoxMouseAdapter extends MouseAdapter{
+	private JButton action_button;
+	private Color border_color;
+	
+	public VoxMouseAdapter(JButton button, Color special_color) {
+		action_button=button;
+		if(special_color!=null){
+			border_color=special_color;
+		} else {
+			border_color = new Color(254, 157, 79);
+		}
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		action_button.setBorderPainted(true);
+		action_button.setBorder(BorderFactory.createLineBorder(border_color,4));
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		action_button.setBorderPainted(true);
+		action_button.setBorder(BorderFactory.createLineBorder(border_color,6));
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		action_button.setBorderPainted(true);
+		action_button.setBorder(BorderFactory.createLineBorder(border_color,4));
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		action_button.setBorderPainted(false);
+	}
+}

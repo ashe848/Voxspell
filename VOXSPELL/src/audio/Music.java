c8404fbb825a3f5cf9bf41b6e907a39b047c380c
ashe848 <ashe848@aucklanduni.ac.nz>
2016-10-21 14:21:10 +1300

    Fixed BUG with multiple audio clips being produced (and uncloseable) when clicking Cancel on level up pop up on highest level. Added audio package and Music singleton class and changed quiz complete accordingly (as well as minor aesthetic enhancements)

---
+++ VOXSPELL/src/audio/Music.java
@@ -0,0 +1,68 @@
+package audio;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ * Singleton class for controlling Music
+ * @author Abby S
+ *
+ */
+public class Music {
+	private static Clip clip;
+	
+	/**
+	 * Sets up background audio if it isn't already playing
+	 * Audio chosen for background celebratory music on quiz completion. 
+	 * Smile (For A Bit) by The Orchestral Movement of 1932 (c) copyright 2009 Licensed under a Creative Commons Attribution (3.0) license. http://dig.ccmixter.org/files/jacksontorreal/22341 Ft: Fourstones
+	 * Free to use in commercial projects.
+	 * 
+	 * Found from http://beta.ccmixter.org/playlist/browse/40708?offset=10
+	 * 
+	 * Original author comment:
+	 * Smile for a bit, because everyone deserves to.
+	 * The second mix that I made for submission. This song is more my poppy, upbeat, sound. If you're wondering, the title comes from a line from a movie wherein one character was urged to smile more. 
+	 * I don't know why but I found that moment stuck with me. In any case, please enjoy.
+	 * 
+	 * @author Abby S
+	 */
+	public static void setupBackgroundMusic(String audio_name) {
+		if(clip==null){
+			File audio_file=new File(audio_name);
+
+			//Modified from http://stackoverflow.com/a/11025384
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(audio_file);
+				AudioFormat format = stream.getFormat();
+				DataLine.Info info = new DataLine.Info(Clip.class, format);
+				clip = (Clip) AudioSystem.getLine(info);
+				clip.open(stream);
+				clip.start();
+			} catch (UnsupportedAudioFileException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (LineUnavailableException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Stops the audio clip
+	 * 
+	 * @author Abby S
+	 */
+	public static void closeClip() {
+		clip.close();
+		clip = null;		
+	}
+}
--- VOXSPELL/src/visiblegui/QuizComplete.java
+++ VOXSPELL/src/visiblegui/QuizComplete.java
@@ -4,17 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -26,6 +17,7 @@ import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
+import audio.Music;
 import vox.VoxMouseAdapter;
 import vox.Voxspell;
 import vox.Voxspell.PanelID;
@@ -44,9 +36,6 @@ import vox.Voxspell.PanelID;
  */
 public class QuizComplete extends JPanel{
 	private Voxspell parent_frame;
-
-	//audio clip
-	private Clip clip;
 
 	//list of words from quiz just completed
 	private ArrayList<String> latest_mastered_words;
@@ -68,7 +57,7 @@ public class QuizComplete extends JPanel{
 		latest_failed_words=parent_frame.getPostQuizHandler().getLatestWordResults().get(2);
 
 		setupTitle();
-		setupAudio();
+		Music.setupBackgroundMusic(parent_frame.getResourceFileLocation()+"quiz_complete_audio.wav");
 		setupTable();
 		determineDisplay();
 		setupHelpButton();
@@ -87,43 +76,6 @@ public class QuizComplete extends JPanel{
 		title.setHorizontalAlignment(SwingConstants.CENTER);
 		title.setBounds(32, 24, 1136, 119);
 		add(title);
-	}
-
-	/**
-	 * Modified from http://stackoverflow.com/a/11025384
-	 * @author Abby S
-	 */
-	private void setupAudio() {
-		/*
-		 * Audio chosen for background celebratory music on quiz completion.
-		 * 
-		 * 
-		 * Smile (For A Bit) by The Orchestral Movement of 1932 (c) copyright 2009 Licensed under a Creative Commons Attribution (3.0) license. http://dig.ccmixter.org/files/jacksontorreal/22341 Ft: Fourstones
-		 * Free to use in commercial projects.
-		 * 
-		 * Found from http://beta.ccmixter.org/playlist/browse/40708?offset=10
-		 * 
-		 * Original author comment:
-		 * Smile for a bit, because everyone deserves to.
-		 * The second mix that I made for submission. This song is more my poppy, upbeat, sound. If you're wondering, the title comes from a line from a movie wherein one character was urged to smile more. 
-		 * I don't know why but I found that moment stuck with me. In any case, please enjoy.
-		 */
-		File audio_file=new File(parent_frame.getResourceFileLocation()+"quiz_complete_audio.wav");
-
-		try {
-			AudioInputStream stream = AudioSystem.getAudioInputStream(audio_file);
-			AudioFormat format = stream.getFormat();
-			DataLine.Info info = new DataLine.Info(Clip.class, format);
-			clip = (Clip) AudioSystem.getLine(info);
-			clip.open(stream);
-			clip.start();
-		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-		}
 	}
 
 	/**
@@ -290,7 +242,7 @@ public class QuizComplete extends JPanel{
 		video_button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				clip.close(); //stops the background music
+				Music.closeClip();//stops the background music
 				parent_frame.changePanel(PanelID.Video);
 			}
 		});
@@ -317,7 +269,7 @@ public class QuizComplete extends JPanel{
 		String[] global = parent_frame.getDataHandler().getGlobalTop().split("\\s+");
 		if(bet_global){
 			//How much bet previous global top by
-			score_results.append("Also bet previous global top by "+(pb_score - Double.parseDouble(global[0]))+"!");
+			score_results.append("Also bet previous global top by "+(pb_score - Double.parseDouble(global[0]))+".\nCongratulations!");
 		} else {
 			//What the global top is (to look up to it)
 			score_results.append("Didn't beat global top of "+global[0]+"\n\nby "+global[1]+"\n\nin "+global[2]);
@@ -353,7 +305,7 @@ public class QuizComplete extends JPanel{
 		back_button.setContentAreaFilled(false);
 		back_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				clip.close(); //stops background music
+				Music.closeClip();//stops background music
 				parent_frame.getFileWritingHandler().writeToProgramFiles();
 				parent_frame.getFileWritingHandler().writeToSettingsFiles();
 				//resets flag for level up
